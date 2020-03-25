e51f4d2f8bf4b9d6560d4a0db9ac31ff7c31e6f9
Siting Shao <christina.shao@berkeley.edu>
2020-03-24 18:18:09 -0700

    Initialize repository

---
+++ scripts/test.js
@@ -0,0 +1,110 @@
+(function(){
+    const quizContainer = document.getElementById('quiz');
+    const resultsContainer = document.getElementById('results');
+    const submitButton = document.getElementById('submit');
+
+    const myQuestions = [
+        {
+            question: "\\(\\frac{x^2}{\\sqrt{2}}\\)?",
+            answers: {
+                a: "\\(2\\)",
+                b: "Wrong",
+                c: "Right"
+            },
+            correctAnswer: "c"
+        },
+        {
+            question: "Question 2?",
+            answers: {
+                a: "Wrong",
+                b: "Wrong",
+                c: "Right"
+            },
+            correctAnswer: "c"
+        },
+        {
+            question: "Question 3?",
+            answers: {
+                a: "Wrong",
+                b: "Wrong",
+                c: "Wrong",
+                d: "Right"
+            },
+            correctAnswer: "d"
+        },
+    ];
+
+    function buildQuiz(){
+        // we'll need a place to store the HTML output
+        const output = [];
+    
+        // for each question...
+        myQuestions.forEach(
+        (currentQuestion, questionNumber) => {
+    
+            // we'll want to store the list of answer choices
+            const answers = [];
+    
+            // and for each available answer...
+            for(letter in currentQuestion.answers){
+    
+                // ...add an HTML radio button
+                answers.push(
+                    `<label>
+                        <input type="radio" name="question${questionNumber}" value="${letter}">
+                        ${letter} :
+                        ${currentQuestion.answers[letter]}
+                    </label>`
+                );
+            }
+    
+            // add this question and its answers to the output
+            output.push(
+                `<div class="question"> ${currentQuestion.question} </div>
+                <div class="answers"> ${answers.join('')} </div>`
+            );
+        }
+        );
+    
+        // finally combine our output list into one string of HTML and put it on the page
+        quizContainer.innerHTML = output.join('');
+    }
+
+    function showResults(){
+        // gather answer containers from our quiz
+        const answerContainers = quizContainer.querySelectorAll('.answers');
+
+        // keep track of user's answers
+        let numCorrect = 0;
+    
+        // for each question...
+        myQuestions.forEach( (currentQuestion, questionNumber) => {
+            // find selected answer
+            const answerContainer = answerContainers[questionNumber];
+            const selector = 'input[name=question'+questionNumber+']:checked';
+            const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+            // if answer is correct
+            if(userAnswer === currentQuestion.correctAnswer){
+                // add to the number of correct answers
+                numCorrect++;
+
+                // color the answers green
+                answerContainers[questionNumber].style.color = 'lightgreen';
+            }
+            // if answer is wrong or blank
+            else{
+                // color the answers red
+                answerContainers[questionNumber].style.color = 'red';
+            }
+        });
+        // show number of correct answers out of total
+        resultsContainer.innerHTML = numCorrect + ' out of ' + myQuestions.length;
+    }
+
+    // display quiz right away
+    buildQuiz();
+
+    // on submit, show results
+    submitButton.addEventListener('click', showResults);
+})();
