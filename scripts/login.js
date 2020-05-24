f27d9d2ddec43a97364e19fc038ec9ed73947005
Christina Shao <christina.shao@berkeley.edu>
2020-05-24 16:32:52 -0700

    check login with users database

--- scripts/login.js
+++ scripts/login.js
@@ -1,7 +1,31 @@
 function check(form){
-    if(form.userid.value == "italented" && form.pswrd.value == "italented"){
-        window.open('test.html')
-    }else{
-        alert("Inncorect username or password")
+    const user = form.userid.value;
+    const pswrd = form.pswrd.value;
+
+    if (user == '') {
+        alert("Please enter a username");
+        return;
     }
+
+    const url = 'https://script.google.com/macros/s/AKfycbyac8xl_AXEjl4OIgFwNGvToDNRf7kWwsl3HO0YNBSvwsjMeSY/exec';
+    let data = {};
+    data['user'] = user;
+    data['pswrd'] = pswrd;
+    $.ajax({
+        url: url,
+        method: "GET",
+        dataType: "json",
+        data: data,
+        success: function (o) {
+            if (o.correct) {
+                openDashboard(user);
+            } else {
+                alert("Inncorect username or password");
+            }
+        }
+    });
+}
+
+function openDashboard(user) {
+    location.href = 'test.html?user=' + user;
 }
