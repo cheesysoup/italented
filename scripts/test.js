138fd40b43fe3d77ae6569a50a0e56e01e9165ab
vivian215 <56425860+vivian215@users.noreply.github.com>
2020-06-28 16:06:53 -0700

    Added short answers feature

--- scripts/test.js
+++ scripts/test.js
@@ -1,8 +1,11 @@
 (function(){
+
+    // set score factors
     const correctPoints = 6;
     const blankPoints = 1.5;
     const wrongPoints = 0;
 
+    // multiple choice questions array
     const questions = [
         {
             question: "\\(\\frac{x^2}{\\sqrt{2}}\\)?",
@@ -31,11 +34,32 @@
                 d: "Right"
             },
             correctAnswer: "d"
-        },
+        }
     ];
 
+    // short answer questions array
+    const shortAnswerQuestions = [
+        {
+            question: "What is 1.25*2?",
+            correctAnswer: ["5/2","2.5"],
+            answer: ""
+        },
+        {
+            question: "How many degrees does a circle have?",
+            correctAnswer: ["360","360 degrees","360 degree"],
+            answer: ""
+        },
+        {
+            question: "What is 1+1?",
+            correctAnswer: ["2"],
+            answer: ""
+        }
+    ];
+
+    // print quiz
     function buildQuiz() {
         let quiz = ``;
+        let numOfMult = 0;
         for (let i = 0; i < questions.length; i++) {
             const question = questions[i];
             let answers = ``;
@@ -49,12 +73,93 @@
             quiz +=
                 `<div class="question"> ${i + 1}. ${question.question} </div>
                  <div class="answers"> ${answers} </div>`;
+
+            numOfMult = i+1;
         }
+        
         $('#quiz').html(quiz);
+
+        buildAllShortAnswers(numOfMult);
     }
 
+    // build short one short answer question
+    function buildShortAnswersOneByOne(questionNumber,numOfMult) {
+        let shortAnswersQuiz = '';
+        let shortAnswersInput = '';
+        
+        shortAnswersInput += 
+            `<label>
+                <input type="text" class="shortAnswersBoxes" name="shortAnswers${questionNumber}">
+            </label>`;
+
+        shortAnswersQuiz += 
+            `<div class="shortAnswerQuestions name="answerBox${questionNumber}"> ${questionNumber+numOfMult+1}. ${shortAnswerQuestions[questionNumber].question} </div>
+            <div class="shortAnswersInput"> ${shortAnswersInput} </div>`
+
+        return shortAnswersQuiz;
+   }
+
+   // build all short answer questions
+   function buildAllShortAnswers(numOfMult) {
+        let allShortAnswers='';
+        for (let i=0; i < shortAnswerQuestions.length; i++) {
+            
+            allShortAnswers += buildShortAnswersOneByOne(i,numOfMult);
+        }
+        $('#shortAnswersQuiz').html(allShortAnswers);
+   }
+
+   // get short answer results
+   function getShortAnswerResults() {
+        let numCorrect = 0;
+        let numBlank = 0;
+        let numWrong = 0;
+
+       let allShortAnswersBoxes = $('#shortAnswersQuiz').find('.shortAnswersBoxes');
+       console.log(allShortAnswersBoxes);
+
+       let allShortAnswers = [];
+       for (let i = 0; i < shortAnswerQuestions.length; i++) {
+            allShortAnswers.push(document.getElementsByName(`shortAnswers${i}`)[0].value);
+       }
+       for (let i = 0; i < allShortAnswers.length; i++) {
+           let answer = allShortAnswers[i];
+           let answerBoxId = "answerBox${questionNumber}";
+
+           let alreadyWrong = false;
+           for (let j = 0; j < shortAnswerQuestions[i].correctAnswer.length; j++) {
+               console.log("ANSWER, CORRECT ANSWER: " + answer + " " + shortAnswerQuestions[i].correctAnswer[j]);
+               if (answer === shortAnswerQuestions[i].correctAnswer[j]) {
+                   console.log("CORRECT");
+                   numCorrect ++;
+                   document.getElementsByName(`shortAnswers${i}`)[0].style.border = '2px solid lightgreen';
+                   break;
+               } else if (answer === '') {
+                   console.log("BLANK");
+                   numBlank++;
+                   document.getElementsByName(`shortAnswers${i}`)[0].style.border = '2px solid yellow';
+                   break;
+               } else {
+                   if (j === shortAnswerQuestions[i].correctAnswer.length-1) {
+                        alreadyWrong = true;
+                        console.log("incorrect");
+                        numWrong++;
+                        document.getElementsByName(`shortAnswers${i}`)[0].style.border = '2px solid red';
+                   }
+                   
+               }
+           }
+       }
+
+       let results = [numCorrect,numBlank,numWrong];
+       console.log(results);
+       return results;
+   }
+
+   // show results
     function showResults() {
         let answerContainers = $('#quiz').find('.answers');
+        console.log(answerContainers);
         let numCorrect = 0;
         let numBlank = 0;
         for (let i = 0; i < questions.length; i++) {
@@ -62,6 +167,7 @@
             let container = answerContainers[i];
             let userAnswer = null;
             const selected = $(container).find(`input[name=question${i}]:checked`);
+            console.log(`input[name=question${i}]:checked`);
             if (selected.length > 0) {
                 userAnswer = selected[0].value;
             }
@@ -75,13 +181,22 @@
                 container.style.color = 'red';
             }
         }
-        let numWrong = questions.length - numCorrect - numBlank;
-        let totalPoints = questions.length * correctPoints;
+
+        getShortAnswerResults();
+
+        let shortAnswerResults = getShortAnswerResults();
+        let numWrong = questions.length - numCorrect - numBlank + shortAnswerResults[2];
+        numCorrect += shortAnswerResults[0];
+        numBlank += shortAnswerResults[1];
+        let totalPoints = (questions.length+shortAnswerQuestions.length) * correctPoints;
         let score = numCorrect * correctPoints + numBlank * blankPoints + numWrong * wrongPoints;
         $('#results').html(`${numCorrect} correct, ${numBlank} blank, and ${numWrong} wrong<br>Your final score is ${score} out of ${totalPoints} possible points`);
     }
 
+    // populate components
     buildQuiz();
-
     $('#submit').click(showResults);
+
 })();    
+
+
