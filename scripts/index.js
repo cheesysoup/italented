9c37de149aaaf22e1181e2787b79d35f2a424b08
William Chu <williamzchu@gmail.com>
2020-11-14 20:44:54 -0800

    header and footer

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,13 @@
 function login() {
     location.href = `pages/login.html`;
 }
+
+function react() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+      $('#login2').show;
+    } else {
+      x.className = "topnav";
+    }
+  }
