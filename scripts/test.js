13d9f3629a5668d42e8d0186e3d273eb6cc0587b
Christina Shao <christina.shao@berkeley.edu>
2020-12-31 22:24:48 -0800

    quiz results

--- scripts/test.js
+++ scripts/test.js
@@ -79,18 +79,20 @@ function exit() {
             for (let i = 0; i < questions.length; i++) {
                 const question = questions[i];
                 let answers = ``;
+                let j = 1;
                 for (answer in question.answers) {
                     answers += 
-                        `<label class="mc-container">
-                            <input type="radio" name="question${i}" value="${answer}">
-                            <span class="mc"></span>
+                        `<label class="mc-container" id="question${i+1}-answer${j}">
+                            <input type="radio" name="question${i+1}" value="${answer}">
+                            <span class="mc open"></span>
                             ${answer}: ${question.answers[answer]}
                         </label>`;
+                    j++;
                 }
                 answers += 
-                    `<label class="mc-container">
-                        <input type="radio" name="question${i}" value="blank" checked="checked">
-                        <span class="mc"></span>
+                    `<label class="mc-container" id="question${i+1}-answer${j}">
+                        <input type="radio" name="question${i+1}" value="blank" checked="checked">
+                        <span class="mc open"></span>
                         Leave blank
                     </label>`;
                 quiz += `<div class="question" id="question${i+1}"> ${i + 1}. ${question.question} </div>`;
@@ -115,7 +117,7 @@ function exit() {
             let shortAnswersInput = '';
             
             shortAnswersInput += 
-                `<label>
+                `<label id="shortAnswerLabel${questionNumber}">
                     <input type="text" class="short-answer shortAnswersBoxes" name="shortAnswers${questionNumber}">
                 </label>`;
 
@@ -129,14 +131,14 @@ function exit() {
         // build all short answer questions
         function buildAllShortAnswers(numOfMult) {
             let allShortAnswers = '';
-            for (let i=0; i < shortAnswerQuestions.length; i++) {
-                allShortAnswers += buildShortAnswersOneByOne(i,numOfMult);
+            for (let i = 0; i < shortAnswerQuestions.length; i++) {
+                allShortAnswers += buildShortAnswersOneByOne(i, numOfMult);
             }
             $('#shortAnswersQuiz').html(allShortAnswers);
             
             totalQuestions = numOfMult + shortAnswerQuestions.length;
             totalPages = Math.ceil(totalQuestions/5);
-            for (let q = pageSize+1; q <= numOfMult + shortAnswerQuestions.length; q++){
+            for (let q = pageSize+1; q <= numOfMult + shortAnswerQuestions.length; q++) {
                 $(`#question${q}`).hide();
                 $(`#image${q}`).hide();
                 $(`#answers${q}`).hide();
@@ -154,23 +156,29 @@ function exit() {
                 allShortAnswers.push(document.getElementsByName(`shortAnswers${i}`)[0].value);
             }
             for (let i = 0; i < allShortAnswers.length; i++) {
-                let answer = allShortAnswers[i];
+                // lock short answer boxes
+                document.getElementsByName(`shortAnswers${i}`)[0].disabled = true;
 
-                let alreadyWrong = false;
+                let answer = allShortAnswers[i];
+                const rect = document.getElementById(`shortAnswerLabel${i}`);
+                const questionText = document.getElementsByName(`answerBox${i}`)[0];
                 for (let j = 0; j < shortAnswerQuestions[i].correctAnswer.length; j++) {
+                    rect.style.width = "330px";
+                    rect.style.height = "50px";
                     if (answer === shortAnswerQuestions[i].correctAnswer[j]) {
                         numCorrect++;
-                        document.getElementsByName(`shortAnswers${i}`)[0].style.border = '2px solid lightgreen';
+                        rect.style.backgroundColor = 'rgb(121, 203, 69, .5)';
                         break;
                     } else if (answer === '') {
                         numBlank++;
-                        document.getElementsByName(`shortAnswers${i}`)[0].style.border = '2px solid yellow';
+                        rect.style.backgroundColor = 'rgb(255, 210, 49, .5)';
+                        // questionText.style.color = "red";
                         break;
                     } else {
                         if (j === shortAnswerQuestions[i].correctAnswer.length-1) {
-                            alreadyWrong = true;
                             numWrong++;
-                            document.getElementsByName(`shortAnswers${i}`)[0].style.border = '2px solid red';
+                            rect.style.backgroundColor = 'rgb(217, 60, 33, 0.5)';
+                            // questionText.style.color = "red";
                         }
                     }
                 }
@@ -182,8 +190,12 @@ function exit() {
 
         // show results
         function showResults() {
+            $('#submit').hide();
+            $('#viewQuizResults').show();
+
+            $('.mc').removeClass('open');
+
             var data = {};
-            let answerContainers = $('#quiz').find('.answers');
             let numCorrect = 0;
             let numBlank = 0;
             let numWrong = 0;
@@ -194,43 +206,83 @@ function exit() {
 
             for (let i = 0; i < questions.length; i++) {
                 const question = questions[i];
-                let quest =  $('#quiz').find(`#answers${i+1}`);
-                quest[0].style.color = 'red';
+                let quest = $('#quiz').find(`#answers${i+1}`);
                 let container = quest[0];
                 let userAnswer = null;
-                const selected = $(container).find(`input[name=question${i}]:checked`);
+                const selected = $(container).find(`input[name=question${i+1}]:checked`);
+                
+                // lock mc buttons
+                for (let j = 0; j < Object.keys(question.answers).length + 1; j++) {
+                    document.getElementsByName(`question${i+1}`)[j].disabled = true;
+                }
+
+                // find num of selected answer choice
+                let selectedNum = null;
+                if (selected.length > 0) {
+                    selectedNum = selected[0].value.charCodeAt(0)-96;
+                }
+
+                // get and style correctAnswerBox (cab)
+                const correctAnswerNum = question.answer.charCodeAt(0)-96;
+                const cab = document.getElementById(`question${i+1}-answer${correctAnswerNum}`);
+                cab.style.display = "inline-block";
+                cab.style.paddingRight = "20px";
+
+                // color the correct/selected answers
                 if (selected.length > 0) {
                     userAnswer = selected[0].value;
                 }
                 if (userAnswer === question.answer) {
                     numCorrect++;
-                    container.style.color = 'lightgreen';
+                    cab.style.backgroundColor = "rgb(121, 203, 69, .5)";
+                    cab.style.boxShadow = "0px 0px 0px 4px rgb(121, 203, 69, .5)";
                 } else if (userAnswer === 'blank') {
                     numBlank++;
-                    container.style.color = 'yellow';
+                    // style selectedBox
+                    const numChoices = Object.keys(question.answers).length;
+                    const sb = document.getElementById(`question${i+1}-answer${numChoices + 1}`);
+                    sb.style.paddingRight = "20px";
+                    sb.style.backgroundColor = "rgb(255, 210, 49, .5)";
+                    sb.style.boxShadow = "0px 0px 0px 4px rgb(255, 210, 49, .5)";
+                    // style correctAnswerBox
+                    cab.style.boxShadow = "0px 0px 0px 4px rgb(121, 203, 69, .5)";
+                    // cab.boxSizing = "border-box";
+                    // questionText.style.color = 'red';
                 } else {
                     numWrong++;
-                    container.style.color = 'red';
+                    // style selectedBox
+                    const sb = document.getElementById(`question${i+1}-answer${selectedNum}`);
+                    sb.style.paddingRight = "20px";
+                    sb.style.backgroundColor = "rgb(217, 60, 33, 0.5)";
+                    sb.style.boxShadow = "0px 0px 0px 4px rgb(217, 60, 33, 0.5)";
+                    // style correctAnswerBox
+                    cab.style.boxShadow = "0px 0px 0px 4px rgb(121, 203, 69, .5)";
+                    // cab.boxSizing = "border-box";  
+                    // questionText.style.color = 'red';
                 }
-
                 data[`Question ${i+1}`] = userAnswer;
             }
 
             getShortAnswerResults();
 
+            // calculate score
             let shortAnswerResults = getShortAnswerResults();
             numCorrect += shortAnswerResults[0];
             numBlank += shortAnswerResults[1];
             numWrong += shortAnswerResults[2];
             let totalPoints = (questions.length + shortAnswerQuestions.length) * correctPoints;
             let score = numCorrect * correctPoints + numBlank * blankPoints + numWrong * wrongPoints;
-            $('#results').html(`${numCorrect} correct, ${numBlank} blank, and ${numWrong} wrong<br>Your final score is ${score} out of ${totalPoints} possible points`);
             
+            let percentage = Math.round(score*100/totalPoints);
+            makePopupBox(numCorrect, numBlank, numWrong, percentage, score, totalPoints);
+
             $("#loadQuiz").show();
             data['Score'] = score;
             data['user'] = localStorage.getItem("username");
             data['pswrd'] = localStorage.getItem("password");
             data['quiz'] = localStorage.getItem("quiz");
+
+            $("#loadQuiz").hide();
             $.ajax({
                 url: 'https://script.google.com/macros/s/AKfycbz7cE2k_h8VMNbfXTiREI5mc-P9xz6hKo59WVHYfk5y7df4GTP8/exec',
                 method: "POST", dataType: "json", data: data,
@@ -245,6 +297,206 @@ function exit() {
                 }
             });
         }
+
+        // draws a slice of the wheel
+        function drawPieSlice(ctx, centerX, centerY, radius, startAngle, endAngle, color) {
+            ctx.fillStyle = color;
+            ctx.beginPath();
+            ctx.moveTo(centerX, centerY);
+            ctx.arc(centerX, centerY, radius, startAngle, endAngle);
+            ctx.closePath();
+            ctx.fill();
+        }
+    
+        var Wheel = function(options) {
+            this.options = options;
+            this.canvas = options.canvas;
+            this.ctx = this.canvas.getContext("2d");
+            this.colors = options.colors;
+
+            // make consts
+            const c = this.ctx;
+            const width = c.canvas.width;
+            const height = c.canvas.height;
+            const degree = Math.PI/180;
+            const checkCircleRadius = 15;
+            const checkCircleY = height/2-7.5;
+            const checkX = width/2;
+            const checkY = checkCircleY+6;
+            const margin = 20;
+            const percent = this.options.percentage;
+
+            // var grd = c.createLinearGradient(0,0,width,0)
+            // grd.addColorStop(0,"red");
+            // grd.addColorStop(.5,"yellow");
+            // grd.addColorStop(1,"green");
+    
+            this.draw = function() {
+                // // draw outside arc
+                // c.beginPath();
+                // c.arc(width/2,height/2,width/2,180*degree,360*degree);
+                // c.fillStyle = grd;
+                // c.fill();
+
+                var total_value = 0;
+                var color_index = 0;
+                for (var categ in this.options.data) {
+                    var val = this.options.data[categ];
+                    total_value += val;
+                }
+    
+                var start_angle = Math.PI;
+                for (categ in this.options.data) {
+                    val = this.options.data[categ];
+                    var slice_angle = Math.PI * val/total_value;
+    
+                    drawPieSlice(
+                        this.ctx,
+                        this.canvas.width/2,
+                        this.canvas.height/2,
+                        Math.min(this.canvas.width/2-margin, this.canvas.height/2-margin),
+                        start_angle,
+                        start_angle+slice_angle,
+                        this.colors[color_index%this.colors.length]
+                    );
+    
+                    start_angle += slice_angle,
+                    color_index++;
+                }
+    
+                // draw gray circle over wheel
+                if (this.options.smallHoleSize) {
+                    drawPieSlice(
+                        this.ctx,
+                        this.canvas.width/2,
+                        this.canvas.height/2,
+                        this.options.smallHoleSize * Math.min(this.canvas.width/2-margin, this.canvas.height/2-margin),
+                        0,
+                        2*Math.PI,
+                        "#DEDEDE"
+                    );
+                }
+                
+                // write out scores
+                const scoreBreakdown = document.getElementById("scoreBreakdown");
+                scoreBreakdown.style.marginLeft = (width/2-90)+"px";
+                scoreBreakdown.style.marginTop = (height/2-20)+"px";
+
+                const fractionScore = document.getElementById("fractionScore");
+                fractionScore.style.marginLeft = (width/2)+"px";
+                fractionScore.style.marginTop = (-15)+"px";
+    
+                // draw triangle for pointer
+                c.translate(checkX, checkY);
+                c.rotate(degree*percent*.01*180-90*degree);
+                c.translate(-checkX, -checkY);
+                c.beginPath();
+                c.moveTo(checkX-7,checkCircleY+3);
+                c.lineTo(checkX+7,checkCircleY+3);
+                c.lineTo(checkX, 0);
+                c.closePath();
+                c.strokeStyle = "#B1B1B1";
+                c.stroke();
+
+                c.fillStyle = "#FFFFFF";
+                c.fill();
+
+                c.translate(checkX, checkY);
+                c.rotate(360*degree-degree*percent*180*0.01+90*degree);
+                c.translate(-checkX, -checkY);
+
+                // draw green check circle
+                drawPieSlice(
+                    c,
+                    width/2,
+                    checkCircleY,
+                    checkCircleRadius,
+                    0,
+                    2*Math.PI,
+                    "#79cb45"
+                );
+    
+                // draw white check
+                c.translate(checkX, checkY);
+                c.rotate(degree*135);
+                c.translate(-checkX, -checkY);
+                c.fillStyle = "rgb(255,255,255)";
+                c.beginPath();
+                c.fillRect(checkX, checkY,3,10);
+                c.fillRect(checkX-15, checkY, 15, 3);
+                c.stroke();
+
+                c.translate(checkX, checkY);
+                c.rotate(degree*225);
+                c.translate(-checkX, -checkY);    
+            }
+        }
+ 
+        // creates the popup score box
+        function makePopupBox(numCorrect, numBlank, numWrong, percentage, score, totalPoints) {
+            // writes out text
+
+            $('#quizNamePopup').html(localStorage.getItem('quiz') + ":");
+
+            const bigBox = document.getElementById('popupBox');
+            bigBox.style.display = "block";
+
+            const bgBox = document.getElementById('bgBox');
+            bgBox.style.display = "block";
+
+            const scoreBreakdown = document.getElementById("scoreBreakdown");
+            scoreBreakdown.innerHTML = numCorrect + " correct, " + numBlank + " blank, " + numWrong + " wrong";
+
+            const fractionScore = document.getElementById("fractionScore");
+            fractionScore.innerHTML = score + "/" + totalPoints;
+
+            const bigScore = document.getElementById("bigScore");
+            bigScore.innerHTML = Math.round(score/totalPoints*100) + "%";
+
+            // create canvas
+            var myCanvas = document.getElementById("myCanvas");
+            myCanvas.width = 450;
+            myCanvas.height = 450;
+            myCanvas.style.left = "40px";
+            myCanvas.style.top = "40px";
+            myCanvas.style.position = "absolute";
+
+            // set data needed for wheel
+            var answerInfo = {
+                "right": numCorrect,
+                "blank": numBlank,
+                "wrong": numWrong
+            }
+
+            // draw wheel
+            var wheel = new Wheel (
+                {
+                    canvas: myCanvas,
+                    data: answerInfo,
+                    colors: ["#79cb45", "#FBEB59", "#d93c21"],
+                    smallHoleSize: 0.35,
+                    percentage: percentage
+                }
+            )
+            wheel.draw();
+        }
+
+        // gets rid of popup box
+        function hidePopupBox() {
+            const bgBox = document.getElementById('bgBox');
+            bgBox.style.display = "none";
+        }
+        
+        // shows popup box
+        function showPopupBox() {
+            const bgBox = document.getElementById('bgBox');
+            bgBox.style.display = "block";
+        }
+
+        //make buttons on popup box clickable
+        $('#reviewQuiz').click(hidePopupBox);
+        $('#popupX').click(hidePopupBox);
+        $('#viewQuizResults').click(showPopupBox);
 
         // populate components
         buildQuiz();
@@ -266,6 +518,7 @@ function exit() {
 
         $('#loadQuiz').hide();
         $('#quiz-container').show();
+        $('#viewQuizResults').hide();
     }
     function nextPage(){
         if (currentPage < totalPages-1){
